91c4af806e3ad268d0828994073740cafb0c3ba0
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-27 16:40:59 +0530

    backend basic node setup and necessary installs

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send('hello world')
+});
+
+app.listen(5100, ()=>{
+    console.log('server running');
+}) 

ae937c596c82bfb04ef25c3472655fc35158b7bd
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-27 20:58:50 +0530

    backend Error handling middleware Statuscode and customerror

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ import morgan from "morgan";
 import mongoose from "mongoose";
 const app = express();
 
+//routers
 import jobRouter from "./routes/jobRouter.js";
+
+//middleware
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -20,10 +24,7 @@ app.use("*", (req, res) => {
   res.status(404).json({ msg: "not found" });
 });
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).json({ msg: "something went wrong" });
-});
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5100;
 try {

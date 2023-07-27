e99b09abd1a881c30b30c4224ec826cac98bfb2f
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-27 18:58:29 +0530

    backend created CRUD apis for jobs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
 const app = express();
+
+import jobRouter from "./routes/jobRouter.js";
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
@@ -10,15 +12,18 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('hello world')
-});
-app.post('/', (req,res)=>{
-    res.json({message:"data received", data:req.body });
+app.use("/api/v1/jobs", jobRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
 });
 
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
+});
 
 const port = process.env.PORT || 5100;
-app.listen(port, ()=>{
-    console.log(`server running on PORT ${port}....`);
-}) 
+app.listen(port, () => {
+  console.log(`server running on PORT ${port}....`);
+});

b4f10147cca84aa40c4d6953e040dd397e1d2579
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-27 19:02:53 +0530

    backend connected to the mongodb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import morgan from "morgan";
+import mongoose from "mongoose";
 const app = express();
 
 import jobRouter from "./routes/jobRouter.js";
@@ -24,6 +25,12 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 5100;
-app.listen(port, () => {
-  console.log(`server running on PORT ${port}....`);
-});
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log(`server running on PORT ${port}....`);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}

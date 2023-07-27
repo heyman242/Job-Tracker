54c749d97f7ae470710424f4a6fe4ed62b0955a8
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-27 17:08:52 +0530

    backend morgan and dotenv port setup

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
-
+import morgan from 'morgan';
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send('hello world')
 });
+app.post('/', (req,res)=>{
+    res.json({message:"data received", data:req.body });
+});
 
-app.listen(5100, ()=>{
-    console.log('server running');
+
+const port = process.env.PORT || 5100;
+app.listen(port, ()=>{
+    console.log(`server running on PORT ${port}....`);
 }) 

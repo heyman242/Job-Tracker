1590aeae73c5a25b7cb12e2e0b1d9bf10ecbf2c2
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-28 19:22:40 +0530

    backend added jwt and httpOnly cookie for login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+} from "../errors/customErrors.js";
+import { createJWT } from "../utils/tokenUtils.js";
 
 export const register = async (req, res) => {
   const isFirstAccount = (await User.countDocuments()) === 0;
@@ -14,5 +19,20 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login");
+  const user = await User.findOne({ email: req.body.email });
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
+  if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
+
+  const token = createJWT({ userId: user._id, role: user.role });
+
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+
+  res.status(StatusCodes.CREATED).json({ msg: "user logged in" });
 };

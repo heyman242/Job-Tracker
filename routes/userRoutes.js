6546246b210f33327e4596fb959e1fffaf626fae
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-28 21:23:15 +0530

    backend updated application stats api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,19 @@
-import {Router} from 'express';
-import { updatedUser, getApplicationStats, getCurrentUser } from '../controllers/userController.js';
-import {validateUpdateUserInput} from '../middleware/validationMiddleware.js'
+import { Router } from "express";
+import {
+  updatedUser,
+  getApplicationStats,
+  getCurrentUser,
+} from "../controllers/userController.js";
+import { validateUpdateUserInput } from "../middleware/validationMiddleware.js";
+import { authorizePermissions } from "../middleware/authMiddleware.js";
 const router = Router();
 
-
-router.get('/current-user', getCurrentUser);
-router.get('/admin/app-stats', getApplicationStats)
+router.get("/current-user", getCurrentUser);
+router.get(
+  "/admin/app-stats",
+  authorizePermissions("admin"),
+  getApplicationStats
+);
 router.patch("/update-user", validateUpdateUserInput, updatedUser);
 
 export default router;

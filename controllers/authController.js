50c04012e020b0393d4298ccaf2d4af6680641db
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-28 18:16:53 +0530

    backend added validation layer for register and bcryptjs for hashing password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,19 @@
-import { StatusCodes } from "http-status-codes"
-import User from "../models/UserModel.js"
+import { StatusCodes } from "http-status-codes";
+import User from "../models/UserModel.js";
+import bcrypt from "bcryptjs";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
-    const user = await User.create(req.body)
-    res.status(StatusCodes.CREATED).json({user});
-}
+  const isFirstAccount = (await User.countDocuments()) === 0;
+  req.body.role = isFirstAccount ? "admin" : "user";
 
-export const login = async (req,res) => {
-    res.send("login")
-}
+  const hashedPassword = await hashPassword(req.body.password);
+  req.body.password = hashedPassword;
+
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: "user created" });
+};
+
+export const login = async (req, res) => {
+  res.send("login");
+};
--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -1,9 +1,9 @@
-import { body,param, validationResult } from "express-validator";
-import { BadRequestError, NotFoundError} from "../errors/customErrors.js";
+import { body, param, validationResult } from "express-validator";
+import { BadRequestError, NotFoundError } from "../errors/customErrors.js";
 import { JOB_STATUS, JOB_TYPE } from "../utils/constants.js";
 import mongoose from "mongoose";
-import Job from '../models/JobModel.js'
-
+import Job from "../models/JobModel.js";
+import User from "../models/UserModel.js";
 
 const withValidationErrors = (validateValues) => {
   return [
@@ -39,4 +39,26 @@ export const validateIdParam = withValidationErrors([
     const job = await Job.findById(value);
     if (!job) throw new NotFoundError(`no job with id : ${value}`);
   }),
+]);
+
+export const validateRegisterInput = withValidationErrors([
+  body("name").notEmpty().withMessage("name is required"),
+  body("email")
+    .notEmpty()
+    .withMessage("email is required")
+    .isEmail()
+    .withMessage("invalid email format")
+    .custom(async (email) => {
+      const user = await User.findOne({ email });
+      if (user) {
+        throw new BadRequestError("email already exists");
+      }
+    }),
+  body("password")
+    .notEmpty()
+    .withMessage("password is required")
+    .isLength({ min: 8 })
+    .withMessage("password must be at least 8 characters"),
+  body("location").notEmpty().withMessage("location is required"),
+  body("lastName").notEmpty().withMessage("lastName is required"),
 ]);

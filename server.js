cd1647cbbeb4d5846f2d5c6cfe23c2db9cdf8930
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-29 15:16:17 +0530

    installed axios and concurrently for running server

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
+
 //routers
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from './routes/authRouter.js';
@@ -21,12 +25,14 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+
 app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/v1/jobs",authenticateUser ,jobRouter);
 app.use("/api/v1/users",authenticateUser ,userRouter);
 app.use('/api/v1/auth', authRouter);
+
 
 
 app.use("*", (req, res) => {

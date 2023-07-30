541f542709524ae6bd73bf9c7124edbaccb9b0cb
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-30 19:02:00 +0530

    backend created show stats functionality for jobstatus and applications

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -8,6 +8,7 @@ import {
   createJob,
   updateJob,
   deleteJob,
+  showStats,
 } from "../controllers/jobController.js";
 
 import {checkForTestUser} from '../middleware/authMiddleware.js'
@@ -16,6 +17,9 @@ router
   .route("/")
   .get(getAllJobs)
   .post(checkForTestUser,validateJobInput, createJob);
+
+
+router.route('/stats').get(showStats)
 router
   .route("/:id")
   .get(validateIdParam, getJob)

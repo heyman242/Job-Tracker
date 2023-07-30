ded7fc9289d33e1b387724b96e58d18b58447c94
Himanshu Bobade <himanshubobade96@gmail.com>
2023-07-30 19:44:41 +0530

    frontend updated stats page which shows pending scheduled declined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -24,6 +24,7 @@ import { action as editJobAction } from "./pages/EditJob";
 import { action as deleteJobAction } from "./pages/DeleteJob";
 import { action as profileAction } from "./pages/Profile";
 import { loader as adminLoader } from "./pages/Admin";
+import { loader as statsLoader } from "./pages/Stats";
 
 const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -66,6 +67,7 @@ const router = createBrowserRouter([
           {
             path: "stats",
             element: <Stats />,
+            loader:statsLoader,
           },
           {
             path: "all-jobs",
